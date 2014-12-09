791957619cde5f0b7972bd7a78e873dfee982ff9
Gio Nikky Gozum Lapid <glapid@unix2.lt.ucsc.edu>
2014-12-08 16:41:20 -0800

    final

---
+++ KeyCollisionException.java
@@ -0,0 +1,10 @@
+// Gio Lapid
+// CS12B
+// PA3 - Dictionary.java
+// KeyCollisionException.java
+
+public class KeyCollisionException extends RuntimeException{
+    public KeyCollisionException(String s){
+	super(s);
+    }
+}
---
+++ KeyNotFoundException.java
@@ -0,0 +1,10 @@
+// Gio Lapid
+// CS12B
+// PA3 - Dictionary.java
+// KeyNotFoundException.java
+
+public class KeyNotFoundException extends RuntimeException{
+    public KeyNotFoundException(String s){
+	super(s);
+    }
+}
